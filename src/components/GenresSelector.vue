acd9276dbb1dd159816ff5ca245af8ea60b9e43d
Al-E-00 <mazzellaalessio6@gmail.com>
2022-06-27 16:33:36 +0200

    genre selected console.log(NOT WORK)

--- src/components/GenresSelector.vue
+++ src/components/GenresSelector.vue
@@ -3,7 +3,7 @@
         <select v-model="genreSelected" class="form-select" aria-label="Default select example">
             <option selected>Select genres</option>
             <option v-for="(genre, i) in selectorInfo" :key="i" :value="genre"
-                @click.prevent="$emit('changeGenre', genre)"> {{ genre }} </option>
+                @click="selectedGenre(genre)"> {{ genre }} </option>
         </select>
     </div>
 </template>
@@ -18,6 +18,11 @@ export default {
     props: {
         selectorInfo: Array
     },
+    methods: {
+        selectedGenre(selected) {
+            console.log(selected);
+        }
+    }
 }
 </script>
 
